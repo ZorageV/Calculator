47fbd4831247afc4f2a85042fb67c6374520a4f6
Manjeet Singh <manjeetazim@gmail.com>
2023-03-20 01:11:40 +0530

    added main layout'

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["*"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'main-color' : "#FFA3FD",
+        'number_button-color': '#191825',
+        'operator_button_color': '#865DFF',
+        'result-color': '#865DFF'
+      },
+      height:{
+          'cal_h': '500px',
+          'button_h': '340px',
+      },
+      width:{
+          'cal_w': '300px',
+      }
+    },
   },
   plugins: [],
 }
